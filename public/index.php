442965f3f4d27e7f5a062be64d7a23c01e4669ec
Slimen <metatidj.slimen@gmail.com>
2021-01-19 23:05:22 +0100

    creation routeur

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use Router\Router;
+
+require '../vendor/autoload.php';
+
+$router = new Router($_GET['url']);
+
+$router->get('/', 'App\Controllers\BlogController@index');
+$router->get('/posts/:id', 'App\Controllers\BlogController@show');
+
+$router->run();
+?>
+
+<H1>Hello</H1>
---
+++ routes/Router.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Router;
+
+class Router{
+    public $url;
+    public $routes = [];
+
+    public function __construct($url){
+        $this->url = trim($url, '/');
+    }
+
+    public function get(string $path, string $action){
+        $this->routes['GET'][] = new Route($path, $action);
+    }
+    
+    public function run(){
+        foreach ($this->routes[$_SERVER['REQUEST_METHOD']] as $route){
+           if($route->matches($this->url)){
+             $route->execute();  
+           };
+        }
+
+        return header('HTTP¨/1.0 404 Not Found du con');
+    }
+
+}
